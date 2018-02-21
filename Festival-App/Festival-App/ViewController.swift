d1cd5743db8a98e99ac86dddd7dc6fef13833899
Duminica Octavian <duminicaoctavian@Duminicas-MacBook-Pro.local>
2018-02-21 19:33:20 +0200

    xcode project setup

---
+++ Festival-App/Festival-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Festival-App
+//
+//  Created by Duminica Octavian on 21/02/2018.
+//  Copyright © 2018 Duminica Octavian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Festival-App/Pods/Target Support Files/Pods-Festival-App/Pods-Festival-App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Festival_App : NSObject
+@end
+@implementation PodsDummy_Pods_Festival_App
+@end
