1ce1456b52406da9d7f24b93616c2c4b40f16366
Duminica Octavian <duminicaoctavian@192-168-0-101.rdsnet.ro>
2018-04-21 21:48:20 +0300

    set up AWS framework

--- Festival-App/Festival-App/Controller/EditProfileVC.swift
+++ Festival-App/Festival-App/Controller/EditProfileVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AWSS3
+import AWSCognito
 
 class EditProfileVC: UIViewController {
     @IBOutlet weak var usernameTxtField: UITextField!
@@ -14,6 +16,8 @@ class EditProfileVC: UIViewController {
     @IBOutlet weak var confirmPasswordTxtField: UITextField!
     @IBOutlet weak var profileImgView: CircleImage!
     @IBOutlet weak var chageProfileBtn: UIButton!
+    
+    let bucketName = "octaviansuniversalbucket"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +28,15 @@ class EditProfileVC: UIViewController {
         let tap = UITapGestureRecognizer(target: self, action: #selector(LoginVC.handleTap))
         
         view.addGestureRecognizer(tap)
+        
+        // Initialize the Amazon Cognito credentials provider
+        
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType:.EUCentral1,
+                                                                identityPoolId:"eu-central-1:63c762b5-ff45-4253-937b-dd9f1a822a3b")
+        
+        let configuration = AWSServiceConfiguration(region:.EUCentral1, credentialsProvider:credentialsProvider)
+        
+        AWSServiceManager.default().defaultServiceConfiguration = configuration
     }
     
     @objc func handleTap() {
@@ -35,10 +48,37 @@ class EditProfileVC: UIViewController {
         let passwordInput = passwordTxtField.text!
         UserDataService.instance.editUser(username: usernameInput, password: passwordInput) { (success) in
             if success {
+                //self.uploadFile(with: "camera", type: "png")
                 self.dismiss(animated: true, completion: nil)
             }
         }
     }
+    
+    func uploadFile(with resource: String, type: String) {
+        let key = "\(resource).\(type)"
+        let localImagePath = Bundle.main.path(forResource: resource, ofType: type)!
+        let localImageUrl = URL(fileURLWithPath: localImagePath)
+        
+        let request = AWSS3TransferManagerUploadRequest()!
+        request.bucket = bucketName
+        request.key = key
+        request.body = localImageUrl
+        request.acl = .publicReadWrite
+        
+        let transferManager = AWSS3TransferManager.default()
+        transferManager.upload(request).continueWith(executor: AWSExecutor.mainThread()) { (task) -> Any? in
+            if let error = task.error {
+                print(error)
+            }
+            if task.result != nil {
+                print("Uploaded \(key)")
+            }
+            
+            return nil
+        }
+    }
+    
+    
     
     @IBAction func onClosePressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
--- Festival-App/Pods/Target Support Files/Pods-Festival-App/Pods-Festival-App-frameworks.sh
+++ Festival-App/Pods/Target Support Files/Pods-Festival-App/Pods-Festival-App-frameworks.sh
@@ -134,12 +134,18 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSCognito/AWSCognito.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSCore/AWSCore.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSS3/AWSS3.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Socket.IO-Client-Swift/SocketIO.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSCognito/AWSCognito.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSCore/AWSCore.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSS3/AWSS3.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Socket.IO-Client-Swift/SocketIO.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
