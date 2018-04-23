8646cbe644f2dd465e762cbee5278eeb6920602e
Duminica Octavian <duminicaoctavian@192-168-0-101.rdsnet.ro>
2018-04-23 13:39:09 +0300

    moved server to heroku

--- Festival-App/Festival-App/Utils/Constants.swift
+++ Festival-App/Festival-App/Utils/Constants.swift
@@ -27,8 +27,8 @@ let PRODUCT_CELL_IDENTIFIER = "productCell"
 let PRODUCT_VC_IDENTIFIER = "MerchVC"
 
 // URL Constants
-let BASE_URL = "http://localhost:3000"
-//let BASE_URL = "https://pacific-anchorage-10639.herokuapp.com"
+// let BASE_URL = "http://localhost:3000"
+let BASE_URL = "https://pacific-anchorage-10639.herokuapp.com"
 let BASE_AWS = "https://s3.eu-central-1.amazonaws.com/octaviansuniversalbucket"
 let URL_GET_ARTISTS = "\(BASE_URL)/artists"
 let URL_GET_PRODUCTS = "\(BASE_URL)/products"
